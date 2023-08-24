29b621d98cb6be8e8fe7a7a0d24bc483bc7d0413
sundaram08 <sundaramdwivedi2003@gmail.com>
2023-08-24 19:20:44 +0530

    Java.tsx added

---
+++ src/app/codeurl/Java.tsx
@@ -0,0 +1,5 @@
+const Java ={
+    1 : "https://api.github.com/repos/sundaram08/Codetypo-codes/contents/LS.java",
+    2 : "https://api.github.com/repos/sundaram08/Codetypo-codes/contents/floor.java"
+}
+export default Java
